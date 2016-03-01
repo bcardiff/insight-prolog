8b9b1bd459d80797cf30ab7559006efae530b242
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-02-29 23:59:42 -0300

    add initial (and ugliest) subst and unify

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -4,3 +4,43 @@
 
 (defn ^:export lorem []
   "ipsum!!!")
+
+(defrecord PAtom [name]) ; Atoms
+(defrecord PVar [name]) ; Variables
+(defrecord PFun [name args]); Functions
+
+(defmulti subst (fn [term substs] [(type term)]))
+
+(defmethod subst [EmptyList] [_ _]
+  [])
+
+(defmethod subst [IndexedSeq] [s substs]
+  (mapv (fn [arg] (subst arg substs)) s))
+
+(defmethod subst [PersistentVector] [s substs]
+  (mapv (fn [arg] (subst arg substs)) s))
+
+(defmethod subst [PAtom] [a _]
+  a)
+
+(defmethod subst [PVar] [a substs]
+  (if (empty? substs)
+    a
+    (let [[[v t] othersubsts] substs]
+      (if (= a v) t (subst a othersubsts)))))
+
+(defmethod subst [PFun] [{:keys [name args]} substs]
+  (PFun. name (mapv (fn [arg] (subst arg substs)) args)))
+
+(defn unify [terms]
+  (if (empty? terms)
+    []
+    (let [ts (peek terms) g (rest terms)]
+      (let [a (first ts) b (second ts) ta (type a) tb (type b)]
+
+        (if (= a b)
+          (unify g) ; if same term, remove it
+          (if (= ta tb PAtom) nil ; if both are atoms at this point, they differ
+            (if (= ta PVar) (cons [a b] (unify (subst g [[a b]])))
+              (if (= tb PVar) (cons [b a] (unify (subst g [[b a]]))) nil))
+          ))))))
--- test-cljs/test.cljs
+++ test-cljs/test.cljs
@@ -3,8 +3,38 @@
 
 (def success 0)
 
+(def A (c/PVar. "A"))
+(def B (c/PVar. "B"))
+(def a (c/PAtom. "a"))
+(def b (c/PAtom. "b"))
+(defn f [& args] (c/PFun. "f" args))
+(defn g [& args] (c/PFun. "g" args))
+
+(defn assert_unify [terms s] (assert (= (c/unify terms) s)))
+
 (defn core-test []
-  (assert (= (c/lorem) "ipsum!!!")))
+  (assert (= (c/lorem) "ipsum!!!"))
+
+  (assert (= (:name A)  "A"))
+  (assert (= (:name a)  "a"))
+
+  (assert (= (c/subst A []) A))
+  (assert (= (c/subst (f A) [[A b]]) (f b)))
+  (assert (= (c/subst (f A B) [[B A]]) (f A A)))
+  (assert (= (c/subst a [[B A]]) a))
+
+  (assert_unify [[a a]] [])
+  (assert_unify [[a b]] nil)
+  (assert_unify [[A b]] [[A b]])
+  (assert_unify [[b A]] [[A b]])
+  (assert_unify [[A A]] [])
+  (assert_unify [[A B]] [[A B]])
+  (assert_unify [[B A]] [[B A]])
+
+  (assert_unify [[B A] [B A]] [[B A]])
+
+  ; (assert_unify [[(f A) (f b)]] [[A b]])
+  )
 
 (defn -main []
   (.log js/console "insight-prolog tests started.")
